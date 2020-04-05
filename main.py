3a1053348103db957cf7fd78dff1bf367880d04c
philongnghoang <philongnghoang12@gmail.com>
2020-04-05 14:45:15 +0700

    update

--- main.py
+++ main.py
@@ -35,6 +35,8 @@ class Streaming(Process):
         self.channel = connection.channel()
         self.channel.queue_declare(queue=self.URL)
         self.channel.queue_declare(queue='result')
+        self.channel.exchange_declare(exchange='check_status', exchange_type='fanout')
+        self.channel.queue_bind(exchange='check_status', queue=self.URL)
         self.rabbit_status = {}
  
         #################################################
@@ -70,7 +72,7 @@ class Streaming(Process):
         new_recv = json.loads(body.decode('utf8'))
         print('=============================')
         print('NEW RECIVE: ',new_recv) 
-
+        print(self.channel.queue_declare(queue=self.URL).method.queue)
         if new_recv['opcode'] == 'whoareu':
             self.child_conn.send({'stream_name':self.name})
 
@@ -82,12 +84,28 @@ class Streaming(Process):
                 self.child_conn.send({'opcode':'settime','state':'done'})
             else:
                 self.child_conn.send({'opcode':'settime','state':'error'})
-            
         elif new_recv['opcode'] == 'stop':
             print('Stopping process : ',self.name) 
             self.Run = False 
+        elif new_recv['opcode'] == 'check':    
+            channel.basic_publish(exchange='', routing_key='send_main', body=json.dumps({"urls":self.URL}))
+
+class Check_alive(Process):
+    def __init__(self, **kwargs):
+        super(Check_alive, self).__init__()
+        credentials = pika.PlainCredentials('admin', 'admin')
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost',credentials=credentials))
+        #connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+        self.channel = connection.channel()
+        self.channel.exchange_declare(exchange='check_status', exchange_type='fanout')
+    def run(self):
+        while True:
+            message = json.dumps({"opcode":'check'})
+            self.channel.basic_publish(exchange='check_status',routing_key='', body=message)
+            print('[x][x][x] Fanout message')
+            time.sleep(30)
+
 def Start_newstreaming(name,URL, start_date, stop_date , FPS):
-    #parent_conn, child_conn = Pipe()
     newstreaming =  Streaming(name,URL, start_date, stop_date, FPS)
     return newstreaming
 
@@ -105,25 +123,40 @@ def callback(ch, method, properties, body):
         Stream_= Start_newstreaming(name_process,name_process,start_date,stop_date,FPS)
         processes[name_process]         = Stream_
         processes[name_process].start()
+
     print('Publish a message to ', name_process,' queue')
     channel.basic_publish(exchange='', routing_key=name_process, body=body)
-       
 
 def recive_mes_process(ch, method, properties, body):
     print(" [x] [x] [x] ")
     status_process = json.loads(body.decode('utf8'))
-    print(status_process)
-
+    if status_process['urls'] not in running_process:
+        running_process.append(status_process['urls'])
+    print("Running process")
+    print(running_process)
+def restart_queue():
+    list_queue = ['/home/philong/Desktop/FTI_wfh/multipro/mapping.mp4','/home/philong/Desktop/FTI_wfh/multipro/video1.mp4']
+    for i in list_queue:
+        channel.queue_delete(queue=i)
 if __name__ == "__main__":
+    
     processes       =   {}
+    running_process = []
     credentials = pika.PlainCredentials('admin', 'admin')
     connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost',credentials=credentials))
     #connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
     channel = connection.channel()
-    
+    #restart_queue()
     channel.queue_declare(queue='mes')
     channel.basic_consume(queue='mes', on_message_callback=callback, auto_ack=True)
-    channel.queue_declare(queue='main')
-    channel.basic_consume(queue='main', on_message_callback=recive_mes_process, auto_ack=True)
+    channel.queue_declare(queue='send_main')
+    channel.basic_consume(queue='send_main', on_message_callback=recive_mes_process, auto_ack=True)
+    #################################################################################
+    p = Check_alive()
+    p.start()
+    print("Running process")
+    print(running_process)
+    #################################################################################
+    
     print(' [*] Waiting for messages. To exit press CTRL+C')
     channel.start_consuming() 
